dbaea1a9382a04bee9c5f540264e2fdd02025f30
Thomas <thomas@stoeckert.us>
2021-04-03 13:40:21 -0400

    Bouncing Ball Demo

--- main.c
+++ main.c
@@ -4,6 +4,10 @@
 #include "graphics.h"
 #include <stdio.h>
 
+Graphics_Context g_sContext;      // Declare our graphics context for the library
+
+#define redLED BIT0
+
 int main(void)
 {
     // Standard MSP430 Behavior
@@ -15,23 +19,54 @@ int main(void)
 	CSCTL3  &= ~(BIT4|BIT5|BIT6);     // Divider = 0
 	CSCTL0_H =  0;                    // Lock CS registers
 
+	// Configure debug LED
+	P1DIR |=  redLED;
+	P1OUT &= ~redLED;
+
 	/////////////////////////////////////////////////////
 	//           Initialize graphics library           //
 	/////////////////////////////////////////////////////
-	Graphics_Context g_sContext;      // Declare our graphics context for the library
-
 	Crystalfontz128x128_Init();       // Initialize our display communication
 
 	Initialize_Graphics(&g_sContext); // Prepare the display with initial commands
 
 	/////////////////////////////////////////////////////
-	//                Draw a temp screen               //
+	//               Initialize update timer           //
 	/////////////////////////////////////////////////////
-
-	Graphics_drawStringCentered(&g_sContext, "Breakout!", AUTO_STRING_LENGTH, 60, 64, OPAQUE_TEXT);
+	// Use SMCLK @8KHz, Divide by 8, in up mode. Clear it, enable the interrupt flag
+	TA0CTL  =  TASSEL_2 | ID_3 | MC_1 | TACLR | TAIE;
+	TA0CCR0 =  33333 - 1;             // Appx. 30.003Hz, or, 33.33ms
+	TA0CTL &= ~TAIFG;                 // Clear interrupt flag
 
 	// Go to sleep
 	_low_power_mode_1();
 
 	return 0;
+}
+
+#pragma vector = TIMER0_A1_VECTOR
+__interrupt void FIXED_UPDATE(void) {
+    static int velocity_x = 1, velocity_y = 2;
+    static int pos_x = 64, pos_y = 64;
+
+    // Clear our old position
+    Graphics_setForegroundColor(&g_sContext, GRAPHICS_COLOR_BLACK);
+    Graphics_fillCircle(&g_sContext, pos_y, pos_x, 6);
+
+    if(pos_x + velocity_x > 127) velocity_x = -velocity_x;
+    if(pos_y + velocity_y > 127) velocity_y = -velocity_y;
+
+    if(pos_x + velocity_x < 0) velocity_x = -velocity_x;
+    if(pos_y + velocity_y < 0) velocity_y = -velocity_y;
+
+    // Calculate our new position
+    pos_x += velocity_x;
+    pos_y += velocity_y;
+
+    Graphics_setForegroundColor(&g_sContext, GRAPHICS_COLOR_WHITE);
+    Graphics_fillCircle(&g_sContext, pos_y, pos_x, 2);
+
+    TA0CTL &= ~TAIFG;
+
+    P1OUT ^= redLED;
 }
